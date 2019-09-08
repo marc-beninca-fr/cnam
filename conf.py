e35faf82465ac54e747626e0c26ee21f93ca18b7
Marc Beninca <cnam@marc-beninca.fr>
2019-09-08 20:15:32 +0200

    build

---
+++ conf.py
@@ -0,0 +1,14 @@
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.todo',
+    'sphinx.ext.imgmath',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+]
+language = 'en'
+html_show_sphinx = False
+html_show_copyright = False
+html_title = 'ToDo'
+smartquotes = False
+todo_include_todos = True
