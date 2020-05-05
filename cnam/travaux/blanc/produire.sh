de1519309b8dc9efdcf1cf3d4c9b1029d49c8c3b
Marc Beninca <cnam@marc-beninca.fr>
2020-05-05 12:15:04 +0200

    produire/commentaires

--- cnam/travaux/blanc/produire.sh
+++ cnam/travaux/blanc/produire.sh
@@ -3,19 +3,19 @@
 FICHIER='présentation'
 DOSSIER='production'
 
-#
+# purger le dossier, si nécessaire
 rm --force --recursive "${DOSSIER}"
-#
+# [re]créer le dossier
 mkdir "${DOSSIER}"
-#
+# 1° passe : indexer le document dans le dossier
 xelatex -output-directory "${DOSSIER}" "${FICHIER}.tex"
-#
+# 2° passe : produire le document dans le dossier
 xelatex -output-directory "${DOSSIER}" "${FICHIER}.tex"
-#
+# signer le document dans le dossier
 gpg --armor --detach-sign "${DOSSIER}/${FICHIER}.pdf"
-#
+# extraire du dossier le document et sa signature
 mv "${DOSSIER}/${FICHIER}".{pdf,pdf.asc} '.'
-#
+# [re]purger le dossier
 rm --force --recursive "${DOSSIER}"
-#
+# vérifier la signature et enregistrer cette vérification
 gpg --verify "${FICHIER}.pdf.asc" 2> "${FICHIER}.pdf.vrf"
