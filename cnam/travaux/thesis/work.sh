c557e57ca6a17e1dacbeb925d21996297dab4a5e
Marc Beninca <cnam@marc-beninca.fr>
2020-10-17 16:45:47 +0200

    work

---
+++ cnam/travaux/thesis/work.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+FILE="$(realpath "${BASH_SOURCE[0]}")"
+DIRECTORY="$(dirname "${FILE}")"
+cd "${DIRECTORY}"
+
+gedit --new-window thesis/* &
+gedit --new-window index.rst build.py presentation/* &
+evince mémoire.pdf présentation.pdf &
